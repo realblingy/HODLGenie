1b16e7b46ce7f2f3a9790e409ab965e507f931a6
James Dang <jamesphidang@gmail.com>
2021-09-24 16:54:19 +1000

    Fixed footer issue and loading

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ const Model = () => {
 const LoadingScreen = () => (
   <div
     style={{
-      width: "100%",
-      height: "100%",
+      width: "100vh",
+      height: "100vw",
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
@@ -216,7 +216,7 @@ function App() {
         >
           {renderBuyOrSell()}
         </div>
-        <footer style={{ marginTop: "6rem", fontSize: "0.7em" }}>
+        <footer style={{ marginTop: "1rem", fontSize: "0.7em" }}>
           DISCLAIMER: THIS IS NOT FINANCIAL ADVICE AND IS FOR ENTERTAINMENT
           PURPOSES ONLY!
         </footer>
